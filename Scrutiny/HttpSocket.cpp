14bccd8baf67dbb04197ca0b9a93fb388254c90b
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-10-19 14:23:36 -0400

    Started seperation of git status

--- Scrutiny/HttpSocket.cpp
+++ Scrutiny/HttpSocket.cpp
@@ -3,120 +3,137 @@
 
 using namespace Scrut;
 
-HttpSocket::HttpSocket(const char* aHostURL, const char* aHostPort)
+HttpSocket::HttpSocket( const char* aHostURL, const char* aHostPort )
 {
 
-	// Copy the host url and port into info
-	strcpy_s(HostURL, strnlen_s(aHostURL, MAX_HOST_LENGTH) + 1, aHostURL);
-	strcpy_s(HostPort, strnlen_s(aHostPort, MAX_HOST_LENGTH) + 1, aHostPort);
+    // Copy the host url and port into info
+    strcpy_s( HostURL, strnlen_s( aHostURL, MAX_HOST_LENGTH ) + 1, aHostURL );
+    strcpy_s( HostPort, strnlen_s( aHostPort, MAX_HOST_LENGTH ) + 1, aHostPort );
 
-	// TODO: Create A socket here sockets
-	// INitalize WinSoc
-	WSADATA WsaDat;
-	if (WSAStartup(MAKEWORD(2, 2), &WsaDat) != 0)
-	{
-		printf("WSA Initialization failed. Error Code : %d\n", WSAGetLastError());
-		return;
-	}
+    // TODO: Create A socket here sockets
+    // INitalize WinSoc
+    WSADATA WsaDat;
+    if ( WSAStartup( MAKEWORD( 2, 2 ), &WsaDat ) != 0 )
+    {
+        printf( "WSA Initialization failed. Error Code : %d\n", WSAGetLastError() );
+        return;
+    }
 
-	strcpy_s(RequestHeader, MAX_REQUEST_LEN, " HTTP/1.1\r\n");
-	
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, "Content-Type: application/json; charset=UTF-8\r\n");
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, "Content-Encoding: identity\r\n");
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36\r\n");
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, "Host: ");
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, " HTTP/1.1\r\n");
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, HostURL);
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, ":");
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, HostPort);
-	strcat_s(RequestHeader, MAX_REQUEST_LEN, "\r\n");
+    InitHeaders();
 
-    ConnectSocket();
+    ConnectSocket( SendSocket );
+    ConnectSocket( RecieveSocket );
+
+    isDone = false;
+
+    // Start a receive thread
+    //Recieve_Thread = std::thread( &HttpSocket::RecieveThread, this );
 
 }
 
 HttpSocket::~HttpSocket()
 {
-	// Clean up the socket here	
-	Disconnect();
+    // Clean up the socket here	
+    Disconnect();
+
+    // Wait for the receive thread to be done
+    isDone = true;
+    RecieveDataCondition.notify_all();
+    Recieve_Thread.join();
 }
 
-const int HttpSocket::ConnectSocket()
+const int HttpSocket::ConnectSocket( SOCKET& aSoc )
 {
-	int iResult = 0;
+    int iResult = 0;
 
-	struct addrinfo *result = NULL,
-		*ptr = NULL,
-		hints;
+    struct addrinfo *result = NULL,
+        *ptr = NULL,
+        hints;
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family		= AF_UNSPEC;
-	hints.ai_socktype	= SOCK_STREAM;
-	hints.ai_protocol	= IPPROTO_TCP;
+    ZeroMemory( &hints, sizeof( hints ) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
 
-	iResult = getaddrinfo(HostURL, HostPort, &hints, &result);
+    iResult = getaddrinfo( HostURL, HostPort, &hints, &result );
 
-	if (iResult != 0)
-	{
-		printf("getaddrinfo failed: %d\n", iResult);
-		WSACleanup();
-		return iResult;
-	}
+    if ( iResult != 0 )
+    {
+        printf( "getaddrinfo failed: %d\n", iResult );
+        WSACleanup();
+        return iResult;
+    }
 
-	ptr = result;
-	// Attempt to create a streaming socket using IP/TCP 
-	Socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-	// Check for errors in the socket creation
-	if (Socket == INVALID_SOCKET)
-	{
-		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return iResult;
-	}
+    ptr = result;
+    // Attempt to create a streaming socket using IP/TCP 
+    aSoc = socket( ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol );
+    // Check for errors in the socket creation
+    if ( aSoc == INVALID_SOCKET )
+    {
+        printf( "Error at socket(): %ld\n", WSAGetLastError() );
+        freeaddrinfo( result );
+        WSACleanup();
+        return iResult;
+    }
+    
+    // Connect to server.
+    iResult = connect( aSoc, ptr->ai_addr, (int) ptr->ai_addrlen );
+    if ( iResult == SOCKET_ERROR )
+    {
+        closesocket( aSoc );
+        aSoc = INVALID_SOCKET;
+        printf( "-- Error with socket conn! --" );
+    }
 
-	// Connect to server.
-	iResult = connect(Socket, ptr->ai_addr, (int)ptr->ai_addrlen);
-	if (iResult == SOCKET_ERROR)
-	{
-		closesocket(Socket);
-		Socket = INVALID_SOCKET;
-		printf("-- Error with socket conn! --");
-	}
+    freeaddrinfo( result );
 
-	freeaddrinfo(result);
+    if ( aSoc == INVALID_SOCKET )
+    {
+        printf( "Unable to connect to server!\n" );
+        WSACleanup();
+        return iResult;
+    }
 
-	if (Socket == INVALID_SOCKET)
-	{
-		printf("Unable to connect to server!\n");
-		WSACleanup();
-		return iResult;
-	}
+    return iResult;
+}
 
-	return iResult;
+void HttpSocket::InitHeaders()
+{
+    strcpy_s( RequestHeader, MAX_REQUEST_LEN, " HTTP/1.1\r\n" );
+
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, "Content-Type: application/json; charset=UTF-8\r\n" );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, "Content-Encoding: identity\r\n" );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36\r\n" );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, "Host: " );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, " HTTP/1.1\r\n" );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, HostURL );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, ":" );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, HostPort );
+    strcat_s( RequestHeader, MAX_REQUEST_LEN, "\r\n" );
+
 }
 
 const int HttpSocket::SendRequest( const char* aMethod, const char* aIndexParam, const char* aMsg )
 {
-	assert(strcmp(aMethod, "GET") || strcmp(aMethod, "POST") || strcmp(aMethod, "PUT") || strcmp(aMethod, "XDELETE"));
-	
-    ConnectSocket();
+    assert( strcmp( aMethod, "GET" ) || strcmp( aMethod, "POST" ) || strcmp( aMethod, "PUT" ) || strcmp( aMethod, "XDELETE" ) );
 
-	char Request[ MAX_REQUEST_LEN ];
+    ConnectSocket( SendSocket );
+
+    char Request[ MAX_REQUEST_LEN ];
     char* reqPtr = Request;
 
     Request[ 0 ] = '\0';
 
     reqPtr = StrCat_NoCheck( reqPtr, aMethod );
-    
+
     reqPtr = StrCat_NoCheck( reqPtr, " " );
     reqPtr = StrCat_NoCheck( reqPtr, aIndexParam );
     reqPtr = StrCat_NoCheck( reqPtr, RequestHeader );
-    
+
     reqPtr = StrCat_NoCheck( reqPtr, "Content-Length: " );
     reqPtr = StrCat_NoCheck( reqPtr, std::to_string( strlen( aMsg ) ).c_str() );
     reqPtr = StrCat_NoCheck( reqPtr, "\r\n" );
-    
+
     reqPtr = StrCat_NoCheck( reqPtr, ConnectionClose );
     reqPtr = StrCat_NoCheck( reqPtr, aMsg );
     reqPtr = StrCat_NoCheck( reqPtr, "\r\n" );
@@ -126,33 +143,36 @@ const int HttpSocket::SendRequest( const char* aMethod, const char* aIndexParam,
 #if defined(DEBUG) | defined(_DEBUG)
     printf( "\t\n========= REQUEST SENT\n\n%s \t\n========= End request send\n\n", Request );
 #endif // DEBUG
-	
-	// Send an initial buffer
-	iResult = send( Socket, Request, ( int )strlen( Request ), 0 );
-	if ( iResult == SOCKET_ERROR ) 
-	{
-		printf( "send failed: %d\n", WSAGetLastError() );
-		Disconnect();
-		return 1;
-	}
+
+    // Send an initial buffer
+    iResult = send( SendSocket, Request, (int) strlen( Request ), 0 );
+    if ( iResult == SOCKET_ERROR )
+    {
+        printf( "send failed: %d\n", WSAGetLastError() );
+        Disconnect();
+        return 1;
+    }
 
 #if defined(DEBUG) | defined(_DEBUG)
     printf( "\t\t ** Bytes Sent: %ld\n", iResult );
 #endif // DEBUG
 
+    // Let the recv thread know that we want to listen!
+    RecieveDataCondition.notify_one();
 
     // I don't _acutally_ need to be waiting for a response from the server for
     // just sending analytics. This will be a lot more preform ant than 
     // waiting for the response 
-    
+
     RecvData();
 
-	return iResult;
+    return iResult;
 }
 
 const int Scrut::HttpSocket::RecvData()
 {
     int iResult = 0;
+    ConnectSocket( RecieveSocket );
 
     // Receive data until the server closes the connection
     int BytesRecieved = 0;
@@ -163,9 +183,7 @@ const int Scrut::HttpSocket::RecvData()
     do
     {
         // receive data from the server's response
-        // Using this socket, put the data we are receiving into the RecieveBuffer
-        // until you get to this buffer length.
-        iResult = recv( Socket, RecieveBuffer, MAX_RECV_BUF_LEN, MSG_WAITALL );
+        iResult = recv( RecieveSocket, RecieveBuffer, MAX_RECV_BUF_LEN, MSG_WAITALL );
 
         // Keep track of how much data we are receive so that we can add a null terminator
         BytesRecieved += iResult;
@@ -177,7 +195,7 @@ const int Scrut::HttpSocket::RecvData()
 #endif
 
     // Add a null terminator to the end of the data we received
-    RecieveBuffer[ BytesRecieved ] = 0;
+    //RecieveBuffer[ BytesRecieved ] = 0;
 
 #if defined(DEBUG) | defined(_DEBUG)
     printf("\n============== Server Response:\n\n%s\n\n=============== End Server Response\n\n",  RecieveBuffer);
@@ -187,12 +205,17 @@ const int Scrut::HttpSocket::RecvData()
 
 void HttpSocket::Disconnect()
 {
-	if (Socket >= 0)
-	{
-		closesocket(Socket);
-	}
+    if ( SendSocket >= 0 )
+    {
+        closesocket( SendSocket );
+    }
 
-	WSACleanup();
+    if ( RecieveSocket >= 0 )
+    {
+        closesocket( RecieveSocket );
+    }
+
+    WSACleanup();
 }
 
 // Because strcat has to look for the null terminator every time, this is not a linear function
@@ -202,6 +225,24 @@ char * Scrut::HttpSocket::StrCat_NoCheck( char * aDest, const char * aSrc )
     while ( *aDest ) aDest++;
     while ( *aDest++ = *aSrc++ );
     return --aDest;
+}
+
+void Scrut::HttpSocket::RecieveThread()
+{
+    printf( "Enter the Receive thread!\n" );
+
+    std::unique_lock<std::mutex> workerLock( RecvDataMutex );
+
+    while ( true )
+    {
+        // Wait for a webRequest to happen to become available
+        RecieveDataCondition.wait( workerLock );
+
+        // Make sure that we don't need to be done now!
+        if ( isDone ) return;
+
+    }
+
 }
 
 /////////////////////////////////////////////////////////////////
--- Scrutiny/HttpSocket.h
+++ Scrutiny/HttpSocket.h
@@ -1,20 +1,24 @@
 #pragma once
 
 #include <string>		// Use strings until we have a minimum viable product
+#include <thread>       // std::thread
+#include <mutex>        // std::mutex
+#include <condition_variable>   // std::conditional_var
+#include <atomic>       // std::atomic
 
 #if defined(_WIN32) || defined(_WIN64)
 
-	#include <assert.h>		// assert macro
-	#include <WinSock2.h>	// WinSock
-	#include <Ws2tcpip.h>
+#include <assert.h>		// assert macro
+#include <WinSock2.h>	// WinSock
+#include <Ws2tcpip.h>
 
-	#pragma comment(lib,"ws2_32.lib") // Winsock Library
+#pragma comment(lib,"ws2_32.lib") // Winsock Library
 
 #elif defined(__linux__)
 
-	// TODO: Do other includes here or the normal socket library
-	#include <sys/types.h>          /* See NOTES */
-	#include <sys/socket.h>
+// TODO: Do other includes here or the normal socket library
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
 
 #endif
 
@@ -24,34 +28,34 @@
 
 namespace Scrut
 {
-	/// <summary>
-	/// Web request class for interfacing with the ELK stack via sockets.
-	/// </summary>
-	/// <author>Ben Hoffman</author>
-	class HttpSocket
-	{
-	public:
+    /// <summary>
+    /// Web request class for interfacing with the ELK stack via sockets.
+    /// </summary>
+    /// <author>Ben Hoffman</author>
+    class HttpSocket
+    {
+    public:
 
-		/// <summary>
-		/// Creates a web request object
-		/// </summary>
-		/// <param name="aHostURL">The Server host URL</param>
-		/// <param name="aHostPort">The server host port</param>
-		HttpSocket(const char* aHostURL, const char* aHostPort);
+        /// <summary>
+        /// Creates a web request object
+        /// </summary>
+        /// <param name="aHostURL">The Server host URL</param>
+        /// <param name="aHostPort">The server host port</param>
+        HttpSocket( const char* aHostURL, const char* aHostPort );
 
-		/// <summary>
-		/// Clean up sockets
-		/// </summary>
-		~HttpSocket();
- 
-		/// <summary>
-		/// Send a web request to the elastic instance using HTTP
-		/// </summary>
-		/// <param name="aMethod">The HTTP method of this request (POST, PUT, GET, etc)</param>
-		/// <param name="aIndexParam">the ELK index you are interest in</param>
-		/// <param name="aMsg">The message you would like to send to the ELK instance</param>
-		/// <returns>Result from the socket attempt</returns>
-		const int SendRequest(const char*  aMethod, const char*  aIndexParam, const char* aMsg);
+        /// <summary>
+        /// Clean up sockets
+        /// </summary>
+        ~HttpSocket();
+
+        /// <summary>
+        /// Send a web request to the elastic instance using HTTP
+        /// </summary>
+        /// <param name="aMethod">The HTTP method of this request (POST, PUT, GET, etc)</param>
+        /// <param name="aIndexParam">the ELK index you are interest in</param>
+        /// <param name="aMsg">The message you would like to send to the ELK instance</param>
+        /// <returns>Result from the socket attempt</returns>
+        const int SendRequest( const char*  aMethod, const char*  aIndexParam, const char* aMsg );
 
         /// <summary>
         /// receive data from the server until we get a response of 0
@@ -67,32 +71,41 @@ namespace Scrut
 
         /** Return the current host URL being used by the socket */
         const char* GetHostURL() const;
-        
-	private:
+
+    private:
 
         /// <summary>
         /// Connect the socket to the Elasticsearch instance
         /// </summary>
         /// <returns>The result of creating the socket</returns>
-        const int ConnectSocket();
+        const int ConnectSocket( SOCKET& aSoc );
 
-		/** The active socket that will communicate with ELK  */
-		SOCKET Socket = INVALID_SOCKET;
+        /// <summary>
+        /// Appends the header info that is sent on each call to the 
+        /// RequestHeader member variable. 
+        /// </summary>
+        void InitHeaders();
 
-		/* Close socket and cleanup WSA if necessary */
-		void Disconnect();
+        /** The active socket that will send data to ELK*/
+        SOCKET SendSocket = INVALID_SOCKET;
 
-		/** The host that this socket will connect to  */
-		char HostURL[MAX_HOST_LENGTH];
-		
-		/** The host port */
-		char HostPort[8];
+        /** The active socket that will receive data from ELK */
+        SOCKET RecieveSocket = INVALID_SOCKET;
 
-		/** Character array for the closing connection */
-		const char * ConnectionClose = "Connection: close\r\n\r\n";
+        /* Close socket and cleanup WSA if necessary */
+        void Disconnect();
 
-		/** The request header for the HTTP request */
-		char RequestHeader[ MAX_REQUEST_LEN ];
+        /** The host that this socket will connect to  */
+        char HostURL[ MAX_HOST_LENGTH ];
+
+        /** The host port */
+        char HostPort[ 8 ];
+
+        /** Character array for the closing connection */
+        const char * ConnectionClose = "Connection: close\r\n\r\n";
+
+        /** The request header for the HTTP request */
+        char RequestHeader[ MAX_REQUEST_LEN ];
 
         /// <summary>
         /// A str cat function that will NOT check for the null terminator from
@@ -104,5 +117,29 @@ namespace Scrut
         /// <returns>The new end position of the destination char*</returns>
         char* StrCat_NoCheck( char* aDest, const char* aSrc );
 
-	};
+        /// <summary>
+        /// Function used to determine if we should be listening for data from 
+        /// the server
+        /// </summary>
+        void RecieveThread();
+
+        std::thread Recieve_Thread;
+
+        /// <summary>
+        /// A mutex determining if the receive is ready
+        /// </summary>
+        std::mutex RecvDataMutex;
+
+        /// <summary>
+        /// Conditional variable to be used when we want to 
+        /// receive data from the server
+        /// </summary>
+        std::condition_variable RecieveDataCondition;
+
+        /// <summary>
+        /// Atomic bool determining if we are done used for closing all threads
+        /// </summary>
+        std::atomic<bool> isDone;
+
+    };
 }
