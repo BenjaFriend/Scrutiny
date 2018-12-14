a945285e74e3a1f75247772a1ada1ca003076c48
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-12-14 14:12:07 -0500

    Cleanup the C# wrapper

--- CSharp_Scrutiny_Testing/Program.cs
+++ CSharp_Scrutiny_Testing/Program.cs
@@ -29,47 +29,22 @@ namespace CSharp_Scrutiny_Testing
         }
     }
 
-    class LibWrapper
-    {
-        [DllImport("Scrutiny.dll")]
-        public static extern IntPtr CreateScrutiny(string aServerAddress, string aServerPort);
 
-        [DllImport("Scrutiny.dll")]
-        public static extern void DestroyScrutiny(IntPtr obj);
-
-        [DllImport("Scrutiny.dll")]
-        public static extern int TestRequest(IntPtr obj);
-
-        [DllImport("Scrutiny.dll")]
-        public static extern void StartReport(IntPtr obj);
-
-        [DllImport("Scrutiny.dll")]
-        public static extern void SendReport(IntPtr obj);
-
-        [DllImport("Scrutiny.dll")]
-        public static extern void ReportFloat(IntPtr Impl, string key, float aToStrFuncPtr);
-
-        [DllImport("Scrutiny.dll")]
-        public static extern void ReportCharacter(IntPtr Impl, string key, string aToStrFuncPtr);
-
-        [DllImport("Scrutiny.dll")]
-        public static extern void ReportCustom(IntPtr Impl, string key, IntPtr aToStrFuncPtr, IntPtr aObject);
-
-    }
 
     class Program
     {
         /** A pointer to the actual object implementation, because of Unity */
-        private static IntPtr Scrutiny;
+        //private static IntPtr ScrutinyInstance;
 
         public delegate string CustomToStringDelegate(IntPtr value);
 
         static void Main(string[] args)
         {
             // Create scrutiny
-            Scrutiny = LibWrapper.CreateScrutiny("127.0.0.1", "9200");
+            //ScrutinyInstance = Scrutiny.LibWrapper.CreateScrutiny("127.0.0.1", "9200");
+            Scrutiny.Init("127.0.0.1", "9200");
 
-            Console.WriteLine("Scrutiny : " + Scrutiny);
+            //Console.WriteLine("Scrutiny : " + ScrutinyInstance);
 
             Vector testVec = new Vector();
             testVec.x = 1.5f;
@@ -79,23 +54,24 @@ namespace CSharp_Scrutiny_Testing
             // Need to keep the delegate around so it doesnt get eaten by the GC
             CustomToStringDelegate myCustomToString = new CustomToStringDelegate(Vector.CustomVectorString);
 
-            LibWrapper.StartReport(Scrutiny);
-            LibWrapper.ReportFloat(Scrutiny, "float_cs", 42.75f);
+            Scrutiny.StartReport();
+            Scrutiny.ReportFloat("float_cs", 42.75f);
 
             GCHandle gch = GCHandle.Alloc(testVec);
 
-            LibWrapper.ReportCustom(
-                Scrutiny,
+            Scrutiny.ReportCustom(                
                 "C# Delegate",
                 Marshal.GetFunctionPointerForDelegate(myCustomToString),
                 GCHandle.ToIntPtr(gch)
                 );
 
-            LibWrapper.SendReport(Scrutiny);
+            Scrutiny.ReportCharacter("Language", "C#");
+
+            Scrutiny.SendReport();
 
             Console.WriteLine("Test vector: " + testVec.ToString());
 
-            int r = LibWrapper.TestRequest(Scrutiny);
+            int r = Scrutiny.TestRequest();
 
             Console.WriteLine("From C++ Unmanaged Dll: " + r.ToString());
 
---
+++ CSharp_Scrutiny_Testing/Scrutiny.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+class Scrutiny
+{
+
+    private class LibWrapper
+    {
+        [DllImport("Scrutiny.dll")]
+        public static extern IntPtr CreateScrutiny(string aServerAddress, string aServerPort);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern void DestroyScrutiny(IntPtr obj);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern int TestRequest(IntPtr obj);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern void StartReport(IntPtr obj);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern void SendReport(IntPtr obj);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern void ReportFloat(IntPtr Impl, string key, float aToStrFuncPtr);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern void ReportCharacter(IntPtr Impl, string key, string aToStrFuncPtr);
+
+        [DllImport("Scrutiny.dll")]
+        public static extern void ReportCustom(IntPtr Impl, string key, IntPtr aToStrFuncPtr, IntPtr aObject);
+
+    }
+
+    #region Fields
+
+    /// <summary>
+    /// The current Instance of Scrutiny
+    /// </summary>
+    private static IntPtr Instance;
+
+    /// <summary>
+    /// Message to send to users when they try and use Scrutiny without calling init
+    /// </summary>
+    private static string UninitalizedMessage =
+        "You must initialize Scrutiny before using it! Call Scrutiny.Init";
+
+    public delegate string CustomToStringDelegate(IntPtr value);
+
+    #endregion
+
+    public static IntPtr Init(string aServerAddress, string aServerPort)
+    {       
+        Instance = LibWrapper.CreateScrutiny(aServerAddress, aServerPort);
+        
+        return Instance;
+    }
+
+    /// <summary>
+    /// Release the current Scrutiny Instance
+    /// </summary>
+    public static void Release()
+    {
+        if (Instance != null)
+        {
+            LibWrapper.DestroyScrutiny(Instance);
+        }
+    }
+
+    /// <summary>
+    /// Get the instance of scrutiny
+    /// </summary>
+    /// <returns>Pointer to the Scrutiny Instance</returns>
+    public static IntPtr GetInstance()
+    {
+        return Instance;
+    }
+
+    /// <summary>
+    /// Attempt to send a test report to Scrutiny to make sure that it 
+    /// it all hooked up correctly. 
+    /// </summary>
+    public static int TestRequest()
+    {
+        Debug.Assert(Instance != null, UninitalizedMessage);
+
+        return LibWrapper.TestRequest(Instance);
+    }
+
+    public static void StartReport()
+    {
+        Debug.Assert(Instance != null, UninitalizedMessage);
+
+        LibWrapper.StartReport(Instance);
+    }
+
+    public static void SendReport()
+    {
+        Debug.Assert(Instance != null, UninitalizedMessage);
+
+        LibWrapper.SendReport(Instance);
+    }
+
+    public static void ReportFloat(string key, float aVal)
+    {
+        Debug.Assert(Instance != null, UninitalizedMessage);
+
+        LibWrapper.ReportFloat(Instance, key, aVal);
+    }
+
+    public static void ReportCharacter(string key, string aVal)
+    {
+        Debug.Assert(Instance != null, UninitalizedMessage);
+
+        LibWrapper.ReportCharacter(Instance, key, aVal);
+    }
+
+    public static void ReportCustom(string key, IntPtr aToStrFuncPtr, IntPtr aObject)
+    {
+        Debug.Assert(Instance != null, UninitalizedMessage);
+
+        LibWrapper.ReportCustom(Instance, key, aToStrFuncPtr, aObject);
+    }
+}
+
